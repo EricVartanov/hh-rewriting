09168b4ef9d29f2b73269453760eaf28bb45379e
EricVartanov <82024638+EricVartanov@users.noreply.github.com>
2021-10-21 13:40:38 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    var rellax = new Rellax('.rellax', {
+        center: true // данное свойство отвечает за корректное позиционирование элементов параллакса
+    });
+
+});
---
+++ js/swiper.js
@@ -0,0 +1,14 @@
+// const headerSwiper = new Swiper('.aaa', {
+//     // Optional parameters
+//     direction: 'horizontal',
+//     loop: true,
+
+
+//     // Navigation arrows
+//     navigation: {
+//         nextEl: '.head__swiper-next',
+//         prevEl: '.head__swiper-prev',
+//     },
+
+
+// });
